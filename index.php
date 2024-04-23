82b235c3288f2f1b49b4329e8017d2fc86d2446a
killerkiller1 <jaredyankeee@gmail.com>
2024-04-23 10:57:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
     <head>
-        <meta charset="<?php bloginfo('charset');?>">
-        <meta name="description" content="<?php blogInfo('description');?>">
+        <meta charset="<?php bloginfo('charset')?>">
+        <meta name="description" content="<?php blogInfo('description')?>">
         <title><?php bloginfo('name')?></title>
-        <?php wp_head();?>
+        <?php wp_head()?>
     </head>
-    <body <?php body_class();?>>
-        <?php wp_body_open();?>
+    <body <?php body_class()?>>
+        <?php wp_body_open()?>
         <h1>this is my index page</h1>
 
 
-        <?php wp_footer();?>
+        <?php wp_footer()?>
     </body>
 </html>
 
