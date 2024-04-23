4059bfce28947023fedcc0cef6ecc65d1e8717f7
killerkiller1 <jaredyankeee@gmail.com>
2024-04-23 10:20:14 -0400

    Revert "Reset theme"
    This reverts commit e93336c430600ec86e8c973c304e2b9f1b82fc67.

---
+++ templates/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <?php wp_head();?> <!-- get scripts ant stylesheets -->
